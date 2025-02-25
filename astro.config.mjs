8cf3f5067771202bd9b424c520e3e5bfa91a44c6
ilmaqui <78042340+ilmaqui@users.noreply.github.com>
2025-02-25 19:37:35 +0100

    refactor: update Tailwind CSS configuration and styles for compatibility with v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()]
-});
+export default defineConfig({ vite: { plugins: [tailwindcss()] } })
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     if ($(document).scrollTop() > 50) {
       $("#menu")
         .css("background-color", "rgba(255, 255, 255, 0.25)")
-        .addClass("shadow-sm backdrop-filter backdrop-blur-md");
+        .addClass("shadow-xs backdrop-filter backdrop-blur-md");
     } else {
       $("#menu")
         .css("background-color", "rgba(255, 255, 255, 0)")
-        .removeClass("shadow-sm backdrop-filter backdrop-blur-md");
+        .removeClass("shadow-xs backdrop-filter backdrop-blur-md");
     }
   });
 });
